434fc3026382ecfa184c9658baef0bea0a2286e6
benanamen <ducatikevin@yahoo.com>
2019-03-04 12:42:58 -0800

    Removed Try/Catch (Use set_exemption_handler in project instead)

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -2,15 +2,19 @@
 
 namespace PerfectApp\Database;
 
-
+/**
+ * Class Database
+ * @package PerfectApp\Database
+ */
 class Database extends \PDO
 {
+    /**
+     * Database constructor.
+     * @param array $config
+     * @param array $options
+     */
     public function __construct(array $config, array $options = array())
     {
-        try {
-            parent::__construct($config['db_type'] . ':host=' . $config['db_host'] . ';dbname=' . $config['db_name'], $config['db_username'], $config['db_password'], $options);
-        } catch (\PDOException $e) {
-            die('ERROR: ' . $e->getMessage());
-        }
+        parent::__construct($config['db_type'] . ':host=' . $config['db_host'] . ';dbname=' . $config['db_name'], $config['db_username'], $config['db_password'], $options);
     }
 }
