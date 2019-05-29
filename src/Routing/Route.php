d0c461d72d61ebb4c59ddb918d3ccfcc4fb37c3a
benanamen <ducatikevin@yahoo.com>
2019-05-29 13:36:29 -0700

    Updated Routing

--- src/Routing/Route.php
+++ src/Routing/Route.php
@@ -24,40 +24,31 @@ class Route
     private $allowedFiles = [];
 
     /**
-     * @var array
-     */
-    private $standardFiles = [];
-
-    /**
      * Route constructor.
      * @param string $page
-     * @param array $standardFiles
      * @param array $allowedFiles
      * @param string $includePath
      */
-    public function __construct(string $page, array $standardFiles, array $allowedFiles, string $includePath)
+    public function __construct(string $page, array $allowedFiles, string $includePath)
     {
         $this->page = $page;
         $this->includePath = $includePath;
         $this->allowedFiles = $allowedFiles;
-        $this->standardFiles = $standardFiles;
     }
 
     /**
-     * @return mixed|string
+     * @return string
      */
-    public function getPage(): string
+    public function getPage()
     {
-        if ($this->page)
-        {
-            $pageBaseName = basename($this->page);
+        $pageBaseName = basename($this->page);
 
-            if (in_array($pageBaseName, $this->allowedFiles) && file_exists($this->includePath . $pageBaseName . '.php'))
-            {
-                return $pageBaseName . '.php';
-            }
-            return $this->standardFiles['404'];
+        if (in_array($pageBaseName, $this->allowedFiles) && file_exists($this->includePath . $pageBaseName . '.php'))
+        {
+            return $pageBaseName . '.php';
         }
-        return $this->standardFiles['default'];
+
+        http_response_code(404);
+        return '404.php';
     }
 }
