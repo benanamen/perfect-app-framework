b4e7b308151b6a8f48051b9192adf147f0ed0f02
benanamen <ducatikevin@yahoo.com>
2020-07-09 12:29:59 -0700

    Added TokenGenerator Class

---
+++ src/Utilities/TokenGenerator.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: Owner
+ * Date: 7/9/2020
+ * Time: 12:15 PM
+ */
+
+namespace PerfectApp\Utilities;
+
+
+class TokenGenerator
+{
+    /**
+     * @return array
+     */
+    final public function generateToken(): array
+    {
+        $raw_token = openssl_random_pseudo_bytes(16);
+        $encoded_token = bin2hex($raw_token);
+        $token_hash = hash('sha256', $raw_token);
+        return array($encoded_token, $token_hash);
+    }
+}
