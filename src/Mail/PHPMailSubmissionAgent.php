1eae54bf52e7f6bb9f6b9b1da74b7353656e7b1e
Kevin <ducatikevin@yahoo.com>
2021-12-16 15:54:38 -0800

    Updated PHPMailSubmissionAgent.php to support HTML Emails

--- src/Mail/PHPMailSubmissionAgent.php
+++ src/Mail/PHPMailSubmissionAgent.php
@@ -4,19 +4,21 @@ namespace PerfectApp\Mail;
 
 /**
  * Class PHPMailSubmissionAgent
+ *
  * @package PerfectApp\Mail
  */
 class PHPMailSubmissionAgent implements MailSubmissionAgent
 {
     /**
-     * @param string $to
-     * @param string $subject
-     * @param string $message
-     * @param string $from
+     * @param  string  $to
+     * @param  string  $subject
+     * @param  string  $message
+     * @param  array   $headers
+     *
      * @return bool
      */
-    final public function send(string $to, string $subject, string $message, string $from): bool
+    final public function send(string $to, string $subject, string $message, array $headers): bool
     {
-        return mail($to, $subject, $message, "From: $from");
+        return mail($to, $subject, $message, implode("\r\n", $headers));
     }
 }
