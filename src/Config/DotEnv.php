87a610d1f5f170b7b7f392809632326b1f408aa1
Kevin <ducatikevin@yahoo.com>
2021-12-16 18:42:16 -0800

    Added DotEnv functionality

---
+++ src/Config/DotEnv.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+//From https://dev.to/fadymr/php-create-your-own-php-dotenv-3k2i
+
+namespace PerfectApp\Config;
+
+use InvalidArgumentException;
+use RuntimeException;
+
+/**
+ * Class DotEnv
+ *
+ * @package PerfectApp\Config
+ */
+class DotEnv
+{
+    /**
+     * The directory where the .env file can be located.
+     *
+     * @var string
+     */
+    protected string $path;
+
+    /**
+     * DotEnv constructor.
+     *
+     * @param  string  $path
+     */
+    public function __construct(string $path)
+    {
+        if(!file_exists($path)) {
+            throw new InvalidArgumentException(sprintf('%s does not exist', $path));
+        }
+        $this->path = $path;
+    }
+
+    /**
+     *
+     */
+    public function load() :void
+    {
+        if (!is_readable($this->path)) {
+            throw new RuntimeException(sprintf('%s file is not readable', $this->path));
+        }
+
+        $lines = file($this->path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach ($lines as $line) {
+
+            if (strpos(trim($line), '#') === 0) {
+                continue;
+            }
+
+            list($name, $value) = explode('=', $line, 2);
+            $name = trim($name);
+            $value = trim($value);
+
+            if (!array_key_exists($name, $_SERVER) && !array_key_exists($name, $_ENV)) {
+                putenv(sprintf('%s=%s', $name, $value));
+                $_ENV[$name] = $value;
+                $_SERVER[$name] = $value;
+            }
+        }
+    }
+}
